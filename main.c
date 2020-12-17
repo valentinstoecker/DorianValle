ded1519aa8c81027f7af87d7d220c8114bd361d2
Valentin Stöcker <v.stoecker@deersoft.de>
2020-12-17 23:33:32 +0100

    sf

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main() {
   string_print(test);
   string_free(test);
   printf("\n");
-  v3_print(v3_new(1, 2, 3));
+  v3_print(v3_new(1, 2, -3));
   printf("\n");
   v3_print(v3_new(10, -20, 30));
   printf("\n");
