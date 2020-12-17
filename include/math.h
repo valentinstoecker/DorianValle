6a9fcecc03d8ec312dd3429ab0a4cefc03f4c7ae
Valentin Stöcker <v.stoecker@deersoft.de>
2020-12-17 21:33:49 +0100

    add v3

--- include/math.h
+++ include/math.h
@@ -1,4 +1,13 @@
-
+#define VECTOR_TYPE double
+#include <math.h>
 typedef struct {
-  double data[3];
+  VECTOR_TYPE data[3];
 } v3;
+
+v3 v3_new(VECTOR_TYPE data[3]);
+
+v3 v3_add(v3 v, v3 w);
+v3 v3_sub(v3 v, v3 w);
+v3 v3_mult(VECTOR_TYPE r, v3 v);
+VECTOR_TYPE v3_abs(v3 v);
+VECTOR_TYPE v3_dist(v3 v, v3 w);
