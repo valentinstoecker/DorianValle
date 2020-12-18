81506f07ec07ecf8b7785619ac83855058d9c1c4
Valentin Stöcker <v.stoecker@deersoft.de>
2020-12-18 22:41:07 +0100

    add main test

--- test.c
+++ test.c
@@ -1,10 +1,12 @@
 #include "test/test.h"
 
+#include "test/main.h"
 #include "test/string.h"
 #include "test/v3.h"
 
 int main() {
   TEST(string_test, "String")
   TEST(v3_test, "V3")
+  TEST(test_main, "Main")
   return 0;
 }
